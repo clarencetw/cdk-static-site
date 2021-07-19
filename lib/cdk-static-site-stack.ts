ce9fd90d862707cb380e40c83539f676d2c06ed3
Clarence <me@clarence.tw>
2021-07-19 13:24:24 +0800

    Add support cloudfront and route 53

--- lib/cdk-static-site-stack.ts
+++ lib/cdk-static-site-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as s3deploy from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as targets from '@aws-cdk/aws-route53-targets';
+import * as acm from '@aws-cdk/aws-certificatemanager';
 
 export class CdkStaticSiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,6 +17,30 @@ export class CdkStaticSiteStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
+
+    const domainName = 'static.cdk.clarence.tw'
+    const hostedZone = new route53.PublicHostedZone(this, 'HostedZone', {
+      zoneName: 'cdk.clarence.tw'
+    });
+    const certificate = new acm.DnsValidatedCertificate(this, 'CrossRegionCertificate', {
+      domainName,
+      hostedZone,
+      region: 'us-east-1',
+    });
+    const cloudfrontTarget = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: { origin: new origins.S3Origin(destinationBucket) },
+      domainNames: [domainName],
+      certificate,
+    });
+
+    new route53.ARecord(this, 'DomainARecord', {
+      recordName: domainName,
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cloudfrontTarget))
+    });
+    new cdk.CfnOutput(this, 'DomainName', {
+      value: `https://${domainName}`
+    })
 
     new s3deploy.BucketDeployment(this, 'HTMLBucketDeployment', {
       sources: [s3deploy.Source.asset('./website')],
